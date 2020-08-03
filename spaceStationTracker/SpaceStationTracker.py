145006eff4f0213c84dc970018a452fced0b21e7
nasir733 <60297068+nasir733@users.noreply.github.com>
2020-08-03 18:44:47 +0500

    not completed not wrking on the prject any more

--- spaceStationTracker/SpaceStationTracker.py
+++ spaceStationTracker/SpaceStationTracker.py
@@ -1,6 +1,16 @@
 import requests
 import webbrowser
-astronaut_data= 'http://api.open-notify.org/astros.json'
+import time
+import turtle
+
+screen = turtle.Screen()
+screen.setup(8192,4096)
+screen.bgpic('map.gif')
+turtle.mainloop()
+
+
+
+'''astronaut_data= 'http://api.open-notify.org/astros.json'
 
 #result=requests.get(astronaut_data).json()
 #print(result)
@@ -9,18 +19,20 @@ astronaut_data= 'http://api.open-notify.org/astros.json'
 #for person in result['people']:
     #print(person['name'] + 'in ' + person['craft'])
 
-
 iss_location= 'http://api.open-notify.org/iss-now.json'
-result=requests.get(iss_location).json()
-print(result)
 
 
+while True:
+    result=requests.get(iss_location).json()
+    #print(result)
+    timestamp= result['timestamp']
+    lat=result['iss_position']['latitude']
+    lon=result['iss_position']['longitude']
+    print( str(timestamp)+ ' '+ lat +' '+ lon)
+    time.sleep(.1)
 
-timestamp= result['timestamp']
-lat=result['iss_position']['latitude']
-lon=result['iss_position']['longitude']
-print( str(timestamp)+ ' '+ lat +' '+ lon)
+
 
 maps=f"http://maps.google.com/?q={lat},{lon}"
 
-webbrowser.open_new_tab(maps)
+webbrowser.open_new_tab(maps)'''
